b5e5d54c5234bf677ce60785d67e0e54d116cc95
TimeForANinja <t.kutscha@yahoo.de>
2025-01-16 00:42:11 +0100

    add simple index.html with a GUI to manually & quickly test PACs

--- app/routes/other.py
+++ app/routes/other.py
@@ -1,10 +1,16 @@
 from apiflask import APIFlask
+from flask import send_from_directory
 
 # imports from other parts of this app
 from routes.schemas.generic_output import GenericOutput
 
 
 def register_other_routes(app: APIFlask):
+    @app.get('/')
+    @app.doc(summary="Serve the index page", description="Serve the \"home\"-page")
+    def serve_index():
+        return send_from_directory('routes', 'index.html')
+
     @app.get('/up')
     @app.doc(summary="Check if the server is up", description="Endpoint that can be used to check if the server is up. It always returns a simple \"success\" message.")
     @app.output(GenericOutput)
