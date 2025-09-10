749e8600cd1a601795ff0178af12ee8ba3fa386d
TimeForANinja <t.kutscha@yahoo.de>
2025-09-10 13:41:19 +0200

    make swagger available without public cdn

--- app/server.py
+++ app/server.py
@@ -1,6 +1,22 @@
-# create flask app
 from apiflask import APIFlask
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
+# create flask app
 app = APIFlask(__name__, "PAC Test Server", version="1.0.0", docs_path="/docs")
+
+# load env variables into app.config
+app.config.from_prefixed_env(prefix='APP')
+
+# allow swagger to be served offline
+app.config['SWAGGER_UI_CSS'] = 'static/css/swagger-ui.css'
+app.config['SWAGGER_UI_BUNDLE_JS'] = 'static/js/swagger-ui-bundle.js'
+app.config['SWAGGER_UI_STANDALONE_PRESET_JS'] = 'static/js/swagger-ui-standalone-preset.js'
+
+# fix for src_ip if used behind a reverse proxy
+if app.config.get('PROXY_FIX', 'false').lower() == 'true':
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
 
 # add routes
 from routes.other import register_other_routes
@@ -9,6 +25,7 @@ from routes.pac import register_pac_routes
 register_pac_routes(app)
 from routes.eval import register_eval_routes
 register_eval_routes(app)
+
 
 # add "status" and "status_code" fields to the default flask errors
 @app.error_processor
